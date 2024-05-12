76198e46604a63281fdcec0fea6577be842a7212
arians-code-hub <arian.codes@gmail.com>
2024-05-12 16:17:45 +0330

    msg

--- src/command/command/run.ts
+++ src/command/command/run.ts
@@ -13,13 +13,11 @@ export default class run extends Command {
         console.log('props',props);
 
         const current = execSync(`pwd`).toString();
-        console.log('current',current);
-        process.exit(0);
 
         if (props['!build'])
             execSync(`cd ${props['!path']} && npm run build`);
 
-        execSync(`node ${props["!path"]}/dist/index.js ${props["!command"]} ${argsToStr(gatherExcept(props, ['!build', '!path','!command']))}`);
+        execSync(`cd ${current} && node ${props["!path"]}/dist/index.js ${props["!command"]} ${argsToStr(gatherExcept(props, ['!build', '!path','!command']))}`);
 
     }
 }
